74cdaa0161b0037fe58b4f6e0e281e4e5ff680e7
Elwaze <eloise.lamontagne@gmail.com>
2020-11-20 13:44:46 +0100

    still a problem with the activate email

--- apps/user/tokens.py
+++ apps/user/tokens.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-# from django.utils import six
+# from django.utils.encoding force_text
 # from six import text_type
 
 
--- apps/user/views.py
+++ apps/user/views.py
@@ -80,6 +80,7 @@ def create_account(request):
                 'user': user,
                 'domain': settings.SITE_LINK,
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                # 'uid': force_text(urlsafe_base64_encode(force_bytes(user.pk))),
                 'token': account_activation_token.make_token(user)
                 # 'link': settings.SITE_LINK
                 }
@@ -124,10 +125,11 @@ def activate(request, uidb64, token):
         user.is_active = True
         user.save()
         login(request, user)
-        # return redirect('home')
-        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+        # return redirect('my_account')
+        return HttpResponse(
+            'L\'activation de votre compte Pur Beurre a été réalisée. vous pouvez maintenant vous connecter.')
     else:
-        return HttpResponse('Activation link is invalid!')
+        return HttpResponse('Le lien d\'activation est invalide, veuillez réessayer !')
 
 # def user_confirmation(request):
 #     token = request.GET.get('token')
