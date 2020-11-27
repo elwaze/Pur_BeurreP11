e8beddb490a8d4990ba3eb7f3c174921ad5ff3d7
Elwaze <eloise.lamontagne@gmail.com>
2020-11-27 14:50:18 +0100

    working on test activate, setting problems of tokens

--- tests/apps/user/tests_views.py
+++ tests/apps/user/tests_views.py
@@ -6,6 +6,7 @@ from apps.user.tokens import account_activation_token
 from django.urls import reverse
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
+import time
 
 
 class TestUserViews(TestCase):
@@ -16,7 +17,7 @@ class TestUserViews(TestCase):
     def setUp(self):
         self.username = 'test@gmail.com'
         self.password = 'test'
-        self.firstname= 'test'
+        self.firstname = 'test'
         self.client = Client()
         self.user = User.objects.create_user(username=self.username, password=self.password)
         self.user.firstname = self.firstname
@@ -77,6 +78,7 @@ class TestUserViews(TestCase):
         user.save()
         print("user.is_active 1")
         print(user.is_active)
+        time.sleep(3)
         response = self.client.get(reverse('activate', kwargs={'uidb64': self.uid, 'token': self.token}))
         self.assertEqual(response.status_code, 200)
         user = User.objects.get(pk=self.user.pk)
